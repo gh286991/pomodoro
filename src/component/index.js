de54efbe18fb6ed22a1aa700ce663c89c6afc6ba
莊雅驛 <jhuangyayi@zhuangyayide-MacBook-Pro.local>
2019-07-27 15:52:36 +0800

    init env with eslint

---
+++ src/component/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+class Index extends Component {
+    static propTypes = {
+
+    };
+
+    constructor(props) {
+      super(props);
+      this.state = {
+      };
+    }
+
+
+    render() {
+      return (
+        <div>
+         HI~~~ Index here  !!
+        </div>
+      );
+    }
+}
+
+export default Index;
