d08e28bcccf78270ed7c7b2988505bafd2e58f74
TomJhuang <jhuangyayi@zhuangyayide-MacBook-Pro.local>
2019-08-04 14:02:13 +0800

    update circle progress bar

--- src/component/index.js
+++ src/component/index.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
+import Timer from './timer';
+// import { ReactComponent as Play } from '../Assets/Play.svg';
+import { ReactComponent as Drop } from '../Assets/drop.svg';
+import Background from '../Assets/background.jpeg';
+import '../Assets/css/main.sass';
 
+const sectionStyle = {
+  backgroundImage: `url(${Background})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+};
 
 class Index extends Component {
     static propTypes = {
@@ -14,10 +24,17 @@ class Index extends Component {
 
 
     render() {
-    //   console.log('test');
       return (
-        <div>
-         HI~~~ Index here  !!
+        <div className="App">
+          <div className="side"> 2019CHALLENGE.THEF2E.COM &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; POMODORO </div>
+          <div className="main" style={sectionStyle}>
+            <div className="todoNow">
+The things to do now
+              <div className="drop"><Drop /></div>
+            </div>
+            <Timer />
+            {/* <Play /> */}
+          </div>
         </div>
       );
     }
---
+++ src/component/timer.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import '../Assets/css/main.sass';
+
+
+class Timer extends Component {
+    static propTypes = {
+
+    };
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        deg: 0,
+        r: 90,
+        strokeDashoffset: 0,
+      };
+    }
+
+    componentDidMount=() => {
+      const { r } = this.state;
+      const strokeDashoffset = 2 * r * Math.PI;
+      this.setState({ strokeDashoffset });
+    }
+
+    setdeg = (e) => {
+      const { r } = this.state;
+      const percent = (e.target.value) * 0.01;
+      const perimeter = 2 * r * Math.PI;
+      const newStrokeDashoffset = perimeter * (1 - percent);
+
+      this.setState({ deg: e.target.value, strokeDashoffset: newStrokeDashoffset });
+    }
+
+
+    render() {
+      const { deg, r, strokeDashoffset } = this.state;
+
+      const style = {
+        transform: `rotate(${deg}deg)`,
+      };
+      return (
+        <div className="circleProgress_wrapper">
+          <svg id="svg" width="200" height="200" viewPort="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <circle id="circle" r={r} cx="100" cy="100" fill="transparent" strokeDasharray={2 * r * Math.PI} strokeDashoffset="0" />
+            <circle
+              id="bar"
+              r={r}
+              cx="100"
+              cy="100"
+              fill="transparent"
+              strokeDasharray={2 * r * Math.PI}
+              strokeDashoffset={strokeDashoffset}
+              transform="rotate(-90, 100, 100)"
+            />
+          </svg>
+          <input value={deg} onChange={this.setdeg} />
+
+        </div>
+      );
+    }
+}
+
+export default Timer;
